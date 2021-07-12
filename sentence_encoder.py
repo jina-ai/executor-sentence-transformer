fc84b14e1bb4e182f66bcbe800485f7af0220e71
Sebastian Lettner <51201318+slettner@users.noreply.github.com>
2021-07-12 13:56:16 +0200

    refactor: use jina commons (#3)
    * refactor: use jina commons
    
    * chore: fix version of commons

--- sentence_encoder.py
+++ sentence_encoder.py
@@ -1,10 +1,11 @@
 __copyright__ = "Copyright (c) 2020-2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-from typing import Optional, Generator, Dict, List
+from typing import Optional, Dict, List, Tuple
 
 from jina import Executor, DocumentArray, requests
 from sentence_transformers import SentenceTransformer
+from jina_commons.batching import get_docs_batch_generator
 
 
 class TransformerSentenceEncoder(Executor):
@@ -18,14 +19,14 @@ class TransformerSentenceEncoder(Executor):
         self,
         model_name: str = 'sentence-transformers/paraphrase-mpnet-base-v2',
         device: str = "cpu",
-        default_traversal_paths: List[str] = None,
+        default_traversal_paths: Tuple = ('r', ),
         default_batch_size=32,
         *args,
         **kwargs
     ):
         super().__init__(*args, **kwargs)
         self.default_batch_size = default_batch_size
-        self.default_traversal_paths = default_traversal_paths or ['r']
+        self.default_traversal_paths = default_traversal_paths
         self.model = SentenceTransformer(model_name, device=device)
 
     @requests
@@ -35,24 +36,13 @@ class TransformerSentenceEncoder(Executor):
         :param docs: documents sent to the encoder. The docs must have `blob` of the shape `256`.
         :param parameters: Any additional parameters for the `encode` function.
         """
-        for batch in self._get_docs_batch_generator(docs, parameters):
+        for batch in get_docs_batch_generator(
+                docs,
+                traversal_path=parameters.get('traversal_paths', self.default_traversal_paths),
+                batch_size=parameters.get('batch_size', self.default_batch_size),
+                needs_attr='text'
+        ):
             texts = batch.get_attributes("text")
             embeddings = self.model.encode(texts)
             for doc, embedding in zip(batch, embeddings):
                 doc.embedding = embedding
-
-    def _get_docs_batch_generator(self, docs: DocumentArray, parameters: Dict):
-        traversal_paths = parameters.get("traversal_paths", self.default_traversal_paths)
-        batch_size = parameters.get("batch_size", self.default_batch_size)
-        flat_docs = docs.traverse_flat(traversal_paths)
-        filtered_docs = DocumentArray(
-            [doc for doc in flat_docs if doc is not None and doc.text is not None]
-        )
-        return _batch_generator(filtered_docs, batch_size)
-
-
-def _batch_generator(
-    data: DocumentArray, batch_size: int
-) -> Generator[DocumentArray, None, None]:
-    for i in range(0, len(data), batch_size):
-        yield data[i: i + batch_size]
