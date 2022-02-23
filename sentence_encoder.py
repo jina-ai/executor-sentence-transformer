260e2d0bf6440a4ae00f689b01a708a19c92d2cf
Nan Wang <nan.wang@jina.ai>
2022-02-23 21:14:45 +0800

    feat: update to jina 3

--- sentence_encoder.py
+++ sentence_encoder.py
@@ -1,48 +1,62 @@
 __copyright__ = "Copyright (c) 2020-2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-from typing import Optional, Dict, List, Tuple
+from typing import Dict, Iterable, Optional
 
-from jina import Executor, DocumentArray, requests
+import torch
+from jina import DocumentArray, Executor, requests
 from sentence_transformers import SentenceTransformer
-from jina_commons.batching import get_docs_batch_generator
 
 
 class TransformerSentenceEncoder(Executor):
     """
     Encode the Document text into embedding.
-
-    :param embedding_dim: the output dimensionality of the embedding
     """
 
     def __init__(
         self,
-        model_name: str = 'sentence-transformers/paraphrase-mpnet-base-v2',
-        device: str = "cpu",
-        default_traversal_paths: Tuple = ('r', ),
-        default_batch_size=32,
+        model_name: str = 'all-MiniLM-L6-v2',
+        traversal_paths: Iterable[str] = ('r',),
+        batch_size: int = 32,
+        device: str = 'cpu',
         *args,
         **kwargs
     ):
+        """
+        :param model_name: The name of the sentence transformer to be used
+        :param device: Torch device to put the model on (e.g. 'cpu', 'cuda', 'cuda:1')
+        :param traversal_paths: Default traversal paths
+        :param batch_size: Batch size to be used in the encoder model
+        """
         super().__init__(*args, **kwargs)
-        self.default_batch_size = default_batch_size
-        self.default_traversal_paths = default_traversal_paths
+        self.batch_size = batch_size
+        self.traversal_paths = traversal_paths
         self.model = SentenceTransformer(model_name, device=device)
 
     @requests
-    def encode(self, docs: Optional[DocumentArray], parameters: Dict, **kwargs):
+    def encode(
+        self, docs: Optional[DocumentArray] = None, parameters: Dict = {}, **kwargs
+    ):
         """
-        Encode all docs with images and store the encodings in the embedding attribute of the docs.
-        :param docs: documents sent to the encoder. The docs must have `blob` of the shape `256`.
+        Encode all docs with text and store the encodings in the ``embedding`` attribute
+        of the docs.
+
+        :param docs: Documents to send to the encoder. They need to have the ``text``
+            attribute get an embedding.
         :param parameters: Any additional parameters for the `encode` function.
         """
-        for batch in get_docs_batch_generator(
-                docs,
-                traversal_path=parameters.get('traversal_paths', self.default_traversal_paths),
-                batch_size=parameters.get('batch_size', self.default_batch_size),
-                needs_attr='text'
+        if docs is None:
+            return
+
+        for batch in docs.traverse_flat(
+            traversal_paths=parameters.get('traversal_paths', self.traversal_paths),
+            filter_fn=lambda doc: len(doc.text) > 0,
+        ).batch(
+            batch_size=parameters.get('batch_size', self.batch_size),
         ):
-            texts = batch.get_attributes("text")
-            embeddings = self.model.encode(texts)
-            for doc, embedding in zip(batch, embeddings):
-                doc.embedding = embedding
+            texts = batch.get_attributes('text')
+
+            with torch.inference_mode():
+                embeddings = self.model.encode(texts)
+                for doc, embedding in zip(batch, embeddings):
+                    doc.embedding = embedding
